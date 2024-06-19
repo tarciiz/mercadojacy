975b91f39409f466865eebb6e4c5244d8a1fcb4b
tarciiz <sidtarcisiosid88@gmail.com>
2024-06-19 18:37:38 -0300

    Updates

---
+++ static/js/952.fecfe7c9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmercadojacy=self.webpackChunkmercadojacy||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.fecfe7c9.chunk.js.map
